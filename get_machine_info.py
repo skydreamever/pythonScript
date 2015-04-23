3964ae5974cd6905d4d25f55916fe28395731b42
dream <dxs9642@gmail.com>
2015-04-23 16:49:57 +0800

    一些自己写的比较方面的python脚本，是关于网络方面的，从今天开始更新

---
+++ get_machine_info.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import socket
+ 
+def print_machine_info():
+	host_name = socket.gethostname()
+	ip_address = socket.gethostbyname(host_name)#通过hostname获得ip地址
+	print("Host Name:%s") % host_name
+	print("IP Address:%s") % ip_address
+
+if __name__ == '__main__':#只有在main中执行的时候执行下面这句话，实际上只有单独调用这个脚本的时候才会执行下面的方法，其他情况下不会执行
+	print_machine_info()
---
+++ ip_addr_format_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import socket
+import sys
+import pdb 
+
+def print_ipaddress(ip_address,is_packed):
+#第一参数是IP地址，第二个参数是is_packed，我这里要求传入数字，如果是7f000001就是packed的了
+	real_ip_addr = ""
+	packed = int(is_packed)
+	if packed:
+		real_ip_addr = socket.inet_ntoa(ip_address)#7f000001 to 127.0.0.1
+	else:
+		real_ip_addr = socket.inet_aton(ip_address)#127.0.0.1 to 7f000001
+	print("IP Address After Change:%s") % real_ip_addr
+
+if __name__ == '__main__':#只有在main中执行的时候执行下面这句话，实际上只有单独调用这个脚本的时候才会执行下面的方法，其他情况下不会执行
+	print_ipaddress(sys.argv[1],sys.argv[2])
